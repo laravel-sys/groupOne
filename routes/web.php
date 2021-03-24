9e44f6b16d5a5ff1f21df47ebfa286fd384008d5
omar_sec_account <67722855+omarabdo6647@users.noreply.github.com>
2021-03-25 01:28:25 +0300

    finish basic return process

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/reservation/returnBook', [ReservationsController::class, 'returnBook']);
-Route::apiResource('reservation', ReservationsController::class);
+Route::get('/reservations/returnBook', [ReservationsController::class, 'returnBook'])->name('returnBook');
+Route::resource('reservations', ReservationsController::class);
 
