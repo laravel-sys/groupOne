84e23464163bc38db1fa9d66449651c7699e2bfc
Aya Amer <60091290@cna-qatar.edu.qa>
2021-03-26 15:25:34 +0300

    admin checkout book for a user

--- resources/views/reservedbooks.blade.php
+++ resources/views/reservedbooks.blade.php
@@ -10,10 +10,10 @@
  <tr style="border-bottom: 1px solid black;" class="row ">
  <th class="col-md-1"> User</th>
  <th class="col-md-1"> Book</th>
- <th class="col-md-2"> start date </th>
+ <!-- <th class="col-md-2"> start date </th>
  <th class="col-md-2"> end date </th>
  <th class="col-md-2"> status </th>
- <th class="col-md-2"> comment </th>
+ <th class="col-md-2"> comment </th> -->
  <th class="col-md-2"> submit </th>
 
  </tr>
@@ -25,32 +25,33 @@
  @method('PUT')
 
  <tr style="border-bottom: 1px solid black;" class="row">
+ @if($reservation->status=="requested")
+
  <td class="col-md-1" >{{$reservation->user_id}}</td>
 
  <td class="col-md-1" >{{$reservation->book_id}}</td>
- <td class="col-md-2" >{{$reservation->startDate}}</td>
+ <!-- <td class="col-md-2" >{{$reservation->startDate}}</td>
  <td class="col-md-2">{{$reservation->endDate}}</td>
- @if($reservation->status=="requested")
  <td class="col-md-2"><select name="status" value="{{$reservation->status}}">
 
  <option value="accept">Accept</option>
- <option value="decline">Decline</option>
-</select>
- </td>
- <td class="col-md-2"> <input  name="comment"  />
-</td>
+ <option value="decline">Decline</option> -->
+    <!-- </select>
+    </td>
+    <td class="col-md-2"> <input  name="comment"  />
+    </td> -->
 
 
  <td class="col-md-2">
 
  <input type="hidden" name="book_id" value="{{$reservation->book_id}}" />
- <input type="hidden" name="startdate" value="{{$reservation->startDate}}" />
- <input type="hidden" name="enddate" value="{{$reservation->endDate}}" />
+ <!-- <input type="hidden" name="startdate" value="{{$reservation->startDate}}" />
+<input type="hidden" name="enddate" value="{{$reservation->endDate}}" /> -->
 
 
  <input type="hidden" name="user_id" value="{{$reservation->user_id}}" />
 
- <button type="submit" class="btn btn-primary">submit</button>
+ <button type="submit" class="btn btn-primary">checkout</button>
 
  </td>@endif
  </tr>
